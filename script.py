c25dd40336f90b838a8f60281a873b251b59723a
peder2911 <pglandsverk@gmail.com>
2021-09-23 11:24:29 +0200

    Made batch metric computation shift a parameter

--- script.py
+++ script.py
@@ -1,8 +1,11 @@
+import sys
 import logging
 from cc_metrics.app import preds_api,users_api,country_api,scheduler_api,ged_api
 from cc_metrics.batch import add_null_predictions ,compute_metrics
 
 logging.basicConfig(level=logging.INFO)
 
-#add_null_predictions(preds_api,country_api,scheduler_api,users_api,-1)
-compute_metrics(ged_api,preds_api,scheduler_api,country_api,0)
+shift = int(sys.argv[1])
+
+add_null_predictions(preds_api,country_api,scheduler_api,users_api,shift)
+compute_metrics(ged_api,preds_api,scheduler_api,country_api,shift)
